06e280f197829df81eda353fdebfeb11aea1c2b9
Ergin <80001281+ergineeer@users.noreply.github.com>
2021-03-17 03:35:20 +0300

    Create bisection.m

---
+++ bisection.m
@@ -0,0 +1,35 @@
+function [xr,Ea,i,elapsedTime] = bisection(f,xl,xu,stoppingErrorCriteria,maxIterations)
+    tic
+    while xl>xu || xl==xu
+        fprintf('\nUpper x point should be greater than lower x point.\n')
+        xl = input('Lower x value:  ');
+        xu = input('Upper x value:  ');
+    end
+    plot(xl:0.01:xu,f(xl:0.01:xu),'k')
+    grid on; hold on
+    xr = (xl+xu)/2;
+    xrPrevious = xr;
+    for i=1:maxIterations
+        xr = (xl+xu)/2;
+        Ea = 100*abs((xr-xrPrevious)/xr);
+        if f(xl)*f(xr)<0
+            xrPrevious = xu;
+            xu = xr;    
+        elseif f(xl)*f(xr)>0
+            xrPrevious = xl;
+            xl = xr; 
+        end
+        if i>1 && Ea<stoppingErrorCriteria && abs(f(xr))<0.001
+            plot(xr,f(xr),'b*')
+            break
+        end
+        plot(xr,f(xr),'go')
+        hold on
+    end
+    if abs(f(xr))>0.001
+        xr = "No Root Found, please change the interval limits.";
+        Ea = "No Root Found, please change the interval limits.";
+    end
+    % title('Bisection Method - f(x) = x^2+x-14')
+    xlabel('x'); ylabel('y(x)')
+    elapsedTime = toc;
